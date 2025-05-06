5986df1853e571988c4a8ed3516847633f989217
Huy Vũ <93518371+huyvu23@users.noreply.github.com>
2025-05-06 22:22:28 +0700

    learn pointer

---
+++ 06mypointers/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Welcome to my pointers study of go lang")
+	x := 10
+	p := &x         // p giữ địa chỉ của x
+	fmt.Println(x)  // 10
+	fmt.Println(*p) // 10 (lấy giá trị tại địa chỉ p)
+
+	*p = 99        // thay đổi giá trị tại địa chỉ p
+	fmt.Println(x) // 99 (x bị thay đổi)
+
+	var y *int // y là một con trỏ trỏ đến kiểu int
+	fmt.Print("address of y: ", &y)
+
+}
